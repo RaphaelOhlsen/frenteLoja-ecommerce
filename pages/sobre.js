479e937a90551e519d96e69abe34372ef02f44d3
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2019-11-12 10:52:51 -0300

    Primeiro commit

---
+++ pages/sobre.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Sobre extends Component {
+  render(){
+    return(
+      <div>
+        <h2>Loja IT - Teste</h2>
+      </div>
+    )
+  }
+}
