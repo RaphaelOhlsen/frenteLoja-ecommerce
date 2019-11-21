ac4f13c83b886e43ba1196637516a1c745541bb3
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2019-11-21 15:34:56 -0300

    Página de Pesquisa - Criando a página de Pesquisa e Componentes

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
+import Layout from '../components/Layout';
+import Cabecalho from '../containers/Cabecalho';
+import ProdutosPesquisa from '../containers/Lista/ProdutosPesquisa';
+import Rodape from '../containers/Rodape';
 export default class Pesquisa extends Component {
   render(){
     return(
-      <div>
-        <h2>Loja IT - Pesquisa</h2>
-      </div>
+      <Layout title="Resultados para Mouse | LOJA IT -  Melhores produtos de Tecnologia" >
+        <Cabecalho />
+        <ProdutosPesquisa />
+        <Rodape />
+      </Layout>
     )
   }
 }
