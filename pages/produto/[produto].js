611c3ed8371dc10bdf26f95870a367b7b4b0a9dd
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2019-11-21 15:46:23 -0300

    Página do Produto - Criando Base da Página e Inicio do Hero

--- pages/produto/[produto].js
+++ pages/produto/[produto].js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 
+import Layout from '../components/Layout';
+import Cabecalho from '../containers/Cabecalho';
+import Produto from '../containers/Lista/Produto';
+import Rodape from '../containers/Rodape';
 export default class Produto extends Component {
   render(){
     return(
-      <div>
-        <h2>Loja IT - Produto</h2>
-      </div>
+      <Layout title="Mouse Gamer 1 | LOJA IT -  Melhores produtos de Tecnologia" >
+        <Cabecalho />
+        <Produto />
+        <Rodape />
+      </Layout>
     )
   }
 }
